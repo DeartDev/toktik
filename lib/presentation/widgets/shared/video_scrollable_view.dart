fab333c94d81f722d65609ad099008908a8239bd
deartdev <deart.94@gmail.com>
2025-12-23 16:43:42 -0500

    Se realizan ajustes en el pageview de la app

---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+
+class VideoScrollableView extends StatelessWidget {
+  const VideoScrollableView({super.key, required this.videos});
+
+  final List<VideoPost> videos;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.green),
+        Container(color: Colors.blue),
+      ],
+    );
+  }
+}
